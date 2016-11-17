e2c1f1136af6946d7d91834ad61c45de8ef70a5b
Seth Guberman <sguberman@gmail.com>
2016-11-17 13:09:44 -0500

    fix a typo in cgi101.py

--- Preview/cgi-bin/cgi101.py
+++ Preview/cgi-bin/cgi101.py
@@ -8,4 +8,4 @@ print('<title>Reply Page</title>')
 if 'user' not in form:
     print('<h1>Who are you?</h1>')
 else:
-    print('<h1>Hell <i>{}</i>!'.format(cgi.escape(form['user'].value)))
+    print('<h1>Hello <i>{}</i>!'.format(cgi.escape(form['user'].value)))
