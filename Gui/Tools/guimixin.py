58e788281d17714147df1ae99be0a3299f40a476
Seth Guberman <sguberman@gmail.com>
2017-01-06 16:06:13 -0500

    start gui tools in ch10

---
+++ Gui/Tools/guimixin.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('C:\\Users\\10121976\\PycharmProjects')
+
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+from PP4E.Gui.Tour.scrolledtext import ScrolledText
+from PP4E.launchmodes import PortableLauncher, System
---
+++ Gui/Tools/widgets.py
@@ -0,0 +1,48 @@
+"""
+##############################################################################
+wrap up widget construction in functions for easier use, based on some
+assumptions (eg, expansion); use **extras kwargs for width, font/color, etc.
+and repack result manually later to override defaults if needed;
+##############################################################################
+"""
+from tkinter import *
+
+
+def frame(root, side=TOP, **extras):
+    widget = Frame(root)
+    widget.pack(side=side, expand=YES, fill=BOTH)
+    if extras:
+        widget.config(**extras)
+    return widget
+
+
+def label(root, side, text, **extras):
+    widget = Label(root, text=text, relief=RIDGE)
+    widget.pack(side=side, expand=YES, fill=BOTH)
+    if extras:
+        widget.config(**extras)
+    return widget
+
+
+def button(root, side, text, command, **extras):
+    widget = Button(root, text=text, command=command)
+    widget.pack(side=side, expand=YES, fill=BOTH)
+    if extras:
+        widget.config(**extras)
+    return widget
+
+
+def entry(root, side, linkvar, **extras):
+    widget = Entry(root, relief=SUNKEN, textvariable=linkvar)
+    widget.pack(side=side, expand=YES, fill=BOTH)
+    if extras:
+        widget.config(**extras)
+    return widget
+
+
+if __name__ == '__main__':
+    app = Tk()
+    frm = frame(app, TOP)
+    label(frm, LEFT, 'SPAM')
+    button(frm, BOTTOM, 'Press', lambda: print('Pushed'))
+    mainloop()
