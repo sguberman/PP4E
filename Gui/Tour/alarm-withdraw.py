24b4ca8aa8b20e775c0d874bfa9cafba8285cdbf
Seth Guberman <sguberman@gmail.com>
2017-01-06 15:01:49 -0500

    complete ch9 alarm examples thry pp587

---
+++ Gui/Tour/alarm-withdraw.py
@@ -0,0 +1,16 @@
+import alarm
+
+
+class Alarm(alarm.Alarm):
+    def repeater(self):
+        self.bell()
+        if self.master.state() == 'normal':
+            self.master.withdraw()
+        else:
+            self.master.deiconify()
+            self.master.lift()
+        self.after(self.msecs, self.repeater)
+
+
+if __name__ == '__main__':
+    Alarm().mainloop()
